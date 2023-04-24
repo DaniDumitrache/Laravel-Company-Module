7bd7f6c0e224236a3addadf9eedb24340cc9f487
Dani Dumitrache <76882185+DaniDumitrache@users.noreply.github.com>
2023-04-24 04:56:25 +0300

    finaly

--- webpack.mix.js
+++
@@ -1,5 +0,0 @@
-const mix = require("laravel-mix");
-
-mix.sass('resources/scss/app.scss', 'public/css');
-mix.styles(["resources/css/app.css"], "public/css/app.css");
-mix.scripts(["resources/js/app.js"], "public/js/app.js");
